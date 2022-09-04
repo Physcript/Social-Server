9dc63430ba14b01bd9d5a302df815e13563bee32
physcript <physcript@gmail.com>
2022-09-04 21:59:05 +0800

    fixed bug followed button

---
+++ build/middleware/follow/functions/check.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.check = void 0;
+function check(req, res, next) {
+    // tofollo
+    next();
+    return;
+}
+exports.check = check;
---
+++ src/middleware/follow/functions/check.ts
@@ -0,0 +1,11 @@
+
+
+import { Request, Response, NextFunction } from 'express'
+export function check (req: Request, res: Response, next: NextFunction) {
+  
+  // tofollo
+
+  next()
+  return
+
+}
